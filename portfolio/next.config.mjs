11a18ae3bab08f08c756a9492962e6a2a9ae0216
Jason O'Dell <jason@vitalflohealth.com>
2024-07-18 11:29:33 -0400

    Adding isProd to edit paths

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['tailwindui.com', 'images.unsplash.com'], // Add your custom domains here
+    domains: ['tailwindui.com', 'images.unsplash.com'],
   },
 }
 
