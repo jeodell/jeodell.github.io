cafeac21189e9e2cc1cad59e035880b602c7b3c1
Jason O'Dell <jason@vitalflohealth.com>
2024-07-18 01:26:28 -0400

    Converting to nextjs project

---
+++ portfolio/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['tailwindui.com', 'images.unsplash.com'], // Add your custom domains here
+  },
+}
+
+export default nextConfig
