6bc21f2031d36987040c3b25ee43360f3579bfba
Jason O'Dell <jason@vitalflohealth.com>
2024-07-18 11:43:03 -0400

    Github actions test 2

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By J
+              By Jbug
             </a>
           </div>
         </div>
