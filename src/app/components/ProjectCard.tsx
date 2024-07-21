0b4281b2c6f021195d0670b666823db7c8853b6f
Jason O'Dell <jason@vitalflohealth.com>
2024-07-21 16:15:29 -0400

    Updating hover style for project card

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -14,7 +14,6 @@ interface ProjectCardProps {
 
 export default function ProjectCard(props: ProjectCardProps) {
   function handleOpenDialog(id: string) {
-    console.log('More Details for ' + id)
     const dialog = document.getElementById(id) as HTMLDialogElement
     if (dialog) dialog.showModal()
     else console.error('Dialog not found')
@@ -23,9 +22,9 @@ export default function ProjectCard(props: ProjectCardProps) {
   return (
     <div className="group relative">
       <img src={props.coverImg} className="w-full rounded-lg shadow-md" alt={props.coverAlt} />
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity group-hover:opacity-100">
+      <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-50 opacity-0 transition-opacity group-hover:opacity-100">
         <button
-          className="rounded-full bg-blue-500 px-6 py-2 text-zinc-200"
+          className="rounded-full bg-jo-light px-6 py-2 text-zinc-200"
           onClick={() => handleOpenDialog(props.dialogId)}
         >
           More Details
